23d2f16abd5662c27effd6fd49b24d3aa860c729
Robin Hsu <robinhsu599+dev@gmail.com>
2015-03-04 01:41:49 +0800

    Initial Commit

---
+++ TDDownloadManager/TDDownloadManager.h
@@ -0,0 +1,13 @@
+//
+//  TDDownloadManager.h
+//  TDDownloadManager
+//
+//  Created by Robin Hsu on 2015/3/4.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDDownloadManager : NSObject
+
+@end
---
+++ TDDownloadManager/TDDownloadManager.m
@@ -0,0 +1,13 @@
+//
+//  TDDownloadManager.m
+//  TDDownloadManager
+//
+//  Created by Robin Hsu on 2015/3/4.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import "TDDownloadManager.h"
+
+@implementation TDDownloadManager
+
+@end
