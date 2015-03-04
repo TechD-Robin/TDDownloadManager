5a76ee74022cd83e425a268413aba96d39b366f7
Robin Hsu <robinhsu599+dev@gmail.com>
2015-03-05 02:17:11 +0800

    add a demo project (empty function).

---
+++ Demo/DemoDownloadManager/DemoDownloadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoDownloadManager
+//
+//  Created by Robin Hsu on 2015/3/5.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
