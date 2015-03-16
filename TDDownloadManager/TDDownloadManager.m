a225ae0a5edfe0d145c1819eb822178305e33f4c
Robin Hsu <robinhsu599+dev@gmail.com>
2015-03-17 01:05:17 +0800

    add a method for download test.

--- TDDownloadManager/TDDownloadManager.m
+++ TDDownloadManager/TDDownloadManager.m
@@ -8,6 +8,91 @@
 
 #import "TDDownloadManager.h"
 
+#import "AFNetworking.h"
+#import "TDFoundation.h"
+
 @implementation TDDownloadManager
 
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
++ ( BOOL ) download:(NSString *)urlString
+{
+    if ( nil == urlString )
+    {
+        return NO;
+    }
+    
+    NSURL                         * url;
+    NSURLRequest                  * urlRequest;
+    NSURLSessionConfiguration     * configuration;
+    NSURLSessionDownloadTask      * downloatTask;
+    AFURLSessionManager           * manager;
+    
+    url                             = [NSURL URLWithString: urlString];
+    configuration                   = [NSURLSessionConfiguration defaultSessionConfiguration];
+    downloatTask                    = nil;
+    manager                         = nil;
+    if ( ( nil == url ) || ( nil == configuration ) )
+    {
+        return NO;
+    }
+    
+    urlRequest                      = [NSURLRequest requestWithURL: url];
+    manager                         = [[AFURLSessionManager alloc] initWithSessionConfiguration: configuration];
+    if ( ( nil == urlRequest ) || ( nil == manager ) )
+    {
+        return NO;
+    }
+    
+    downloatTask                    = [manager downloadTaskWithRequest: urlRequest progress: nil destination: ^NSURL * ( NSURL * targetPath, NSURLResponse * response )
+    {
+        NSString                  * downloadFilename;
+        NSString                  * file;
+        NSString                  * extension;
+        NSURL                     * destationURL;
+        
+        destationURL                = nil;
+        downloadFilename            = [response suggestedFilename];
+        if ( nil == downloadFilename )
+        {
+            //  do something ...
+            return nil;
+        }
+        
+        file                        = [downloadFilename stringByDeletingPathExtension];
+        extension                   = [downloadFilename pathExtension];
+        downloadFilename            = TDGetPathForDirectories( TDTemporaryDirectory, file, extension, file );
+        if ( nil == downloadFilename )
+        {
+            //  do something ...
+            return nil;
+        }
+        
+        destationURL                = [NSURL URLWithString: downloadFilename];
+        return destationURL;
+    }
+    completionHandler:  ^( NSURLResponse * response, NSURL * filePath, NSError * error )
+    {
+        ;
+    }];
+    
+    
+    
+    
+    
+    return YES;
+}
+//  --------------------------------
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+
+
 @end
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+
+
+
